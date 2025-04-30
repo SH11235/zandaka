8d79607b655a4c24ef22c2b0cda9ef08addaa830
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2025-05-01 04:33:55 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-	return (
-		<>
-			<h1>残高管理</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>残高管理</h1>
+    </>
+  );
 }
 
 export default App;
